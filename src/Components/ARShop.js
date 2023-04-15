09613e7e623d570d194039ef9ee408b23b191983
Demon-14 <aditya1442002@gmail.com>
2023-04-15 21:58:03 +0530

    Changes in  cart

--- src/Components/ARShop.js
+++ src/Components/ARShop.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Item from './item'
+import '../Components/ARShop.css'
 
 export default function ARShop(props) {
   return (
     <>
-    <div></div>
-
+    <button style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
+<span className='badge badge-warning' id='lblCartCount'>10</span></button>
     <div className='container d-flex'>
     {props.items.map((item)=>{
       return(
