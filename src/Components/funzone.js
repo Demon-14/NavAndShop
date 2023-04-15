23318dadca9c99995e7b4bc95e480cdf7a8f5b13
shivamverma29 <shivamverma2908@gmail.com>
2023-04-15 15:14:50 +0530

    added fun , home , buttons,css done by hkac(nanav)

---
+++ src/Components/funzone.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../fun.css'
+function funzone() {
+  return (
+    <div>
+        <div className="wrapper-fun">
+      <button className='fun-buy'>Buy Tickets</button>
+      <button className='fun-nav'>Navigate</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default funzone
