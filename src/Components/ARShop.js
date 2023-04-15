02c82596453a898819b09846fdfba5860833eb49
shivamverma29 <shivamverma2908@gmail.com>
2023-04-16 00:45:43 +0530

    srivastava

--- src/Components/ARShop.js
+++ src/Components/ARShop.js
@@ -1,22 +1,26 @@
 import React,{useState} from 'react'
+import { useContext } from 'react'
 import Item from './item'
 import '../Components/ARShop.css'
-import { cartItems } from './item'
+import { Link } from 'react-router-dom'
 
+var car = {1 : 0, 2 : 0, 3 : 0}
 export default function ARShop(props) {
+  const [cartItems,setCart]=useState(car);
+  const [val,setVal]=useState(0)
   var count = 0
   for (const prop in cartItems) {
     count += cartItems[prop];
   }
   return (
     <>
-    <button style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
-<span className='badge badge-warning' id='lblCartCount'>10</span></button>
+    <Link to='/cart' style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
+<span className='badge badge-warning' id='lblCartCount'>{val}</span></Link>
     <div className='container d-flex'>
     {props.items.map((item)=>{
       return(
       <>
-      <Item data={item}/>
+      <Item data={item} val={val} setVal={setVal} cartItems={cartItems} setCart={setCart}/>
       </>
       )
     })}
