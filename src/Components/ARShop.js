9fc11bc92accb7acf84babbb4427f563f45f5099
Demon-14 <aditya1442002@gmail.com>
2023-04-15 22:36:36 +0530

    ARShop changed

--- src/Components/ARShop.js
+++ src/Components/ARShop.js
@@ -1,12 +1,16 @@
 import React,{useState} from 'react'
 import Item from './item'
 import '../Components/ARShop.css'
+import { Link } from 'react-router-dom'
 
 export default function ARShop(props) {
   return (
     <>
-    <button style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
-<span className='badge badge-warning' id='lblCartCount'>10</span></button>
+    {/* {console.log(props.items[0])} */}
+    <Link to={{
+          pathname: "/cart",
+        }} style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
+<span className='badge badge-warning' id='lblCartCount'>10</span></Link>
     <div className='container d-flex'>
     {props.items.map((item)=>{
       return(
