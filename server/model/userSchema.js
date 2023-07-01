a30524ecb8ef16ad23c55d1db1a01c45d7efe6ae
kanavchawla <kchawla1_be21@thapar.edu>
2023-07-02 01:33:23 +0530

    added saving user to database made a post request

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,15 +1,19 @@
-const mongoose=require('mongoose')
-const Schema=mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema=new Schema({
-    email:{
-        type:String,
-        required:true,
-    },
-    pwd:{
-        type:String,
-        required:true,
-    }
-})
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  pwd: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports=mongoose.model('user',userSchema)
+module.exports = mongoose.model("User", userSchema);
