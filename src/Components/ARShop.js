93b7f9b7280152d2f06e3f6974e1b4666b1589ce
kanavchawla <kchawla1_be21@thapar.edu>
2023-07-03 16:20:08 +0530

    Added buttons will add fnctionality later

--- src/Components/ARShop.js
+++ src/Components/ARShop.js
@@ -1,34 +1,49 @@
-import React,{useState} from 'react'
-import { useContext } from 'react'
-import Item from './item'
-import '../Components/ARShop.css'
-import { Link } from 'react-router-dom'
-import '../fun.css'
+import React, { useState } from "react";
+import { useContext } from "react";
+import Item from "./item";
+import "../Components/ARShop.css";
+import { Link } from "react-router-dom";
+import "../fun.css";
 
-
-var car = {1 : 0, 2 : 0, 3 : 0}
+var car = { 1: 0, 2: 0, 3: 0 };
 export default function ARShop(props) {
-  const [cartItems,setCart]=useState(car);
-  const [val,setVal]=useState(0)
-  car=cartItems
-  var count = 0
+  const [activeButton, setactiveButton] = useState("Male");
+  const [cartItems, setCart] = useState(car);
+  const [val, setVal] = useState(0);
+  car = cartItems;
+  var count = 0;
   for (const prop in cartItems) {
     count += cartItems[prop];
   }
   return (
-    <div className='funbg'>
-    <Link to='/cart' style={{'position':'relative','left':'1470px'}}><i className="fa" style={{'font-size':'30px'}}>&#xf07a;</i>
-<span className='badge badge-warning' id='lblCartCount'>{val}</span></Link>
-    <div className='container d-flex'>
-    {props.items.map((item)=>{
-      return(
-      <>
-      <Item data={item} val={val} setVal={setVal} cartItems={cartItems} setCart={setCart}/>
-      </>
-      )
-    })}
+    <div className="funbg">
+      <Link to="/cart" style={{ position: "relative", left: "1470px" }}>
+        <i className="fa" style={{ "font-size": "30px" }}>
+          &#xf07a;
+        </i>
+        <span className="badge badge-warning" id="lblCartCount">
+          {val}
+        </span>
+      </Link>
+      <button onClick={() => setactiveButton("Men")}>Men</button>
+      <button onClick={() => setactiveButton("Women")}>Women</button>
+      <button onClick={() => setactiveButton("Children")}>Children</button>
+      <div className="container d-flex">
+        {props.items.map((item) => {
+          return (
+            <>
+              <Item
+                data={item}
+                val={val}
+                setVal={setVal}
+                cartItems={cartItems}
+                setCart={setCart}
+              />
+            </>
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-export {car}
+export { car };
